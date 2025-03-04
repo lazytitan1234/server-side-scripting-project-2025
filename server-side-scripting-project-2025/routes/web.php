63eaf8942d0702aee22ebb02ea03088f7d6f39af
lazytitan1234 <gaminglazytitan@gmail.com>
2025-03-04 17:44:18 +0000

    Updated the Routes inside of web.php with the Route for colleges and students

--- server-side-scripting-project-2025/routes/web.php
+++ server-side-scripting-project-2025/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\CollegeController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('colleges.index');
 });
+
+Route::resource('colleges', CollegeController::class);
+
+Route::resource('students', StudentController::class);
